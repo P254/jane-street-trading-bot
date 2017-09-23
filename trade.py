9c3b982d801732c1f65bb354138bddd9dbec7029
Ubuntu <ubuntu@bot-pmpplusplus.uiuc-2017.js-etc>
2017-09-23 16:51:45 +0000

    x

--- trade.py
+++ trade.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 import sys
 import socket
 import json
+import datetime
+import time
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,11 +19,18 @@ def write(exchange, obj):
 def read(exchange):
     return json.loads(exchange.readline())
 
-def main():
-    exchange = connect()
-    write(exchange, {"type": "hello", "team": "TEAMNAME"})
-    hello_from_exchange = read(exchange)
-    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
 
+def main():
+    timeid = str(datetime.datetime.now()).split(" ")[1].replace(":","").split(".")[0]
+    exchange = connect()
+    print(timeid)
+    write(exchange, {"type": "hello", "team": "PMPPLUSPLUS"})
+    print("the exchange replied" , read(exchange),file=sys.stderr)
+    write(exchange, {"type": "add", "order_id": timeid , "symbol": 'BOND', "dir": "BUY", "price": 997, "size": 1})
+    while True: 
+        hello_from_exchange = read(exchange)
+        print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+        time.sleep(3)
+	
 if __name__ == "__main__":
     main()
