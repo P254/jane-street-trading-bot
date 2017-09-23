fbf7ea92e37301474e2b243caa45bb6a607bd398
bachu2 <bachu2@illinois.edu>
2017-09-23 14:19:32 -0500

    prod

--- trade.py
+++ trade.py
@@ -13,7 +13,7 @@ currentSellOrders = {}
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("production", 20000))
+    s.connect(("production-exch-PMPPLUSPLUS", 20000))
     return s.makefile('rw', 1)
 
 def write(exchange, obj):
